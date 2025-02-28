1a7e4dc436e7f19b794d709d83e384fd6bda1fdc
B Jayodha <76960054+BeminduJayodha@users.noreply.github.com>
2025-03-01 00:06:19 +0530

    Update index.jsx

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,6 +1,6 @@
 import { View, Text, ImageBackground, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
-import Colors from '../../Constant/Colors'
+import Colors from '../../constant/Colors'
 import { useRouter } from 'expo-router'
 
 
